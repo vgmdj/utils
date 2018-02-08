59615ae6183d0f6297bbeb910c47fcfe5808f295
wangrui <wangrui@rongshu.tech>
2018-02-08 17:28:08 +0800

    新增md5加密方式

---
+++ encrypt/md5.go
@@ -0,0 +1,27 @@
+package encrypt
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"golang.org/x/crypto/scrypt"
+	"io"
+)
+
+//SaltPwd 加盐加密
+func SaltPwd(pwd string, salt string) string {
+	for i := 0; i < 10; i++ {
+		if k, err := scrypt.Key([]byte(pwd), []byte(salt), 16384, 8, 1, 32); err == nil {
+			m5 := md5.New()
+			m5.Write(k)
+			return hex.EncodeToString(m5.Sum(nil))
+		}
+	}
+	return salt + pwd
+}
+
+func Md5(text string) string {
+	hashMd5 := md5.New()
+	io.WriteString(hashMd5, text)
+	return fmt.Sprintf("%x", hashMd5.Sum(nil))
+}
