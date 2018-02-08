d5df38d5334914490c1ff4144df4fcd0aab6172c
wangrui <wangrui@rongshu.tech>
2018-02-08 17:28:43 +0800

    新增每日油价接口调用方式

---
+++ lifeApi/oilPrice.go
@@ -0,0 +1,47 @@
+package oil
+
+import (
+	"github.com/vgmdj/utils/dispatch"
+	"log"
+)
+
+const (
+	Baseurl    = "http://apis.eolinker.com/common/oil/getOilPriceToday?"
+	ProductKey = ""
+)
+
+type ResOilPrice struct {
+	StatusCode string `json:"statusCode"` //状态码
+	Desc       string `json:"desc"`       //状态码说明
+	Result     result `json:"result"`
+}
+
+type result struct {
+	List    []list `json:"list"`
+	RetCode int    `json:"ret_code"`
+}
+type list struct {
+	Ct   string `json:"ct"`
+	P0   string `json:"p0"`
+	P89  string `json:"p89"`
+	P90  string `json:"p90"`
+	P92  string `json:"p92"`
+	P93  string `json:"p93"`
+	P95  string `json:"p95"`
+	P97  string `json:"p97"`
+	Prov string `json:"prov"`
+}
+
+func GetPriceToday(prov ...string) (*ResOilPrice, error) {
+	res := new(ResOilPrice)
+	values := make(map[string]string)
+	values["productKey"] = ProductKey
+	err := dispatch.PostFrom(Baseurl, res, values, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return res, nil
+
+}
