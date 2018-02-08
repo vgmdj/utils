a844e49eaf8f4d5cd95a8d66b1b6d683245322bb
wangrui <wangrui@rongshu.tech>
2018-02-08 17:25:19 +0800

    新增gbk与utf8的编码转换

---
+++ chars/encoding.go
@@ -0,0 +1,16 @@
+package chars
+
+import "github.com/axgle/mahonia"
+
+func ConvertGbkToUtf8(src string) string {
+	return convertToString(src, "gbk", "utf8")
+}
+
+func convertToString(src string, srcCode string, tagCode string) string {
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
+}
