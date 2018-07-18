dc883d13b676365694e23959b89655dbe5834c05
wangrui <228226928@qq.com>
2018-07-18 18:35:53 +0800

    update sql.go

--- db/sql.go
+++ db/sql.go
@@ -3,7 +3,9 @@ package db
 import (
 	"fmt"
 	"github.com/vgmdj/utils/chars"
+	"github.com/vgmdj/utils/logger"
 	"strconv"
+	"strings"
 )
 
 type OP string
@@ -30,21 +32,44 @@ func Attach(sql string, query interface{}, data interface{}, op OP) string {
 	return attach(sql, query, data, op, " ")
 }
 
-func Limit(sql string, l, o string) string {
-	var (
-		limit, _  = strconv.Atoi(l)
-		offset, _ = strconv.Atoi(o)
-	)
+func Limit(sql string, pageCount, pageIndex interface{}) string {
+	var count, index int
+
+	switch pageCount.(type) {
+	default:
+		logger.Error("invalid type ")
+		return sql
+	case string:
+		count, _ = strconv.Atoi(pageCount.(string))
+	}
+
+	switch pageIndex.(type) {
+	default:
+		logger.Error("invalid type ")
+		return sql
+	case string:
+		index, _ = strconv.Atoi(pageIndex.(string))
+	}
+
+	limit, offset := LimitQuery(count, index)
+
+	return fmt.Sprintf(" %s limit %d, %d ", sql, limit, offset)
+
+}
+
+func LimitQuery(pageCount, pageIndex int) (limit int, offset int) {
+	limit = pageCount
+	offset = limit * (pageIndex - 1)
 
 	if limit == 0 {
-		return sql
+		return 0, 0
 	}
 
-	if offset == 0 {
-		return fmt.Sprintf(" %s limit %d ", sql, limit)
+	if offset <= 0 {
+		return limit, 0
 	}
 
-	return fmt.Sprintf(" %s limit %d, %d ", sql, offset, limit)
+	return limit, offset
 
 }
 
@@ -53,7 +78,16 @@ func attach(sql string, query interface{}, data interface{}, op OP, relation str
 		return sql
 	}
 
-	sql += fmt.Sprintf(" %s %v %v '%v' ", relation, query, op, data)
+	sql = strings.Replace(sql, "\t", " ", -1)
+	sql = strings.Replace(sql, "\n", " ", -1)
+	sql = strings.Replace(sql, "\r", " ", -1)
+
+	if strings.Contains(strings.ToLower(sql), " where ") || sql == "" {
+		sql = fmt.Sprintf(" %s %s %v %v '%v' ", sql, relation, query, op, data)
+		return sql
+	}
+
+	sql = fmt.Sprintf(" %s where %s %v %v '%v' ", sql, relation, query, op, data)
 	return sql
 }
 
