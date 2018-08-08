d771a64b007f4feb85ebb3e156c8a02e47cec493
wangrui <228226928@qq.com>
2018-08-08 10:15:10 +0800

    update test case

--- sms/sms_test.go
+++ sms/sms_test.go
@@ -20,6 +20,10 @@ func TestRLYun(t *testing.T) {
 
 }
 
+/*
+用于测试
+{{first.DATA}} 订单商品：{{keyword1.DATA}} 订单编号：{{keyword2.DATA}} 支付金额：{{keyword3.DATA}} 支付时间：{{keyword4.DATA}} {{remark.DATA}}
+*/
 func TestWeChat(t *testing.T) {
 	params := make(map[string]interface{})
 	params["appid"] = "wx6ddf008341937de1"
@@ -49,6 +53,38 @@ func TestWeChat(t *testing.T) {
 	wxsms.SetDefaultTemplate(template)
 	err = wxsms.SendMsg("omHgCwm_DFWghRVayaJ35AggBLG8",
 		"first", "k1", "k2", "k3", "k4", "remark")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+}
+
+/*
+加油卡充值成功通知
+{{first.DATA}} 网点：{{keyword1.DATA}} 交易类型：{{keyword2.DATA}} 交易金额：{{keyword3.DATA}} 交易日期：{{keyword4.DATA}} {{remark.DATA}}
+*/
+func TestWeChat1(t *testing.T) {
+	params := make(map[string]interface{})
+	params["appid"] = "wx6ddf008341937de1"
+	params["secret"] = "3aefe696e17fa29ca0e1ad14c8ec36ee"
+
+	wxsms := SMSFactory{SMS_WECHAT}.NewSMSClient(params)
+	template := Template{
+		TemplateId: "gCaF3HNukgVqDX8rPy0Etj6owwfrR1uReBywx9a6A_8",
+		Keys:       []string{"first", "keyword1", "keyword2", "keyword3", "keyword4", "remark"},
+		Color: map[string]string{
+			"first":    "#0000CD",
+			"keyword1": "#0000FF",
+			"keyword2": "#00BFFF",
+			"keyword3": "#4682B4",
+			"keyword4": "#1E90FF",
+			"remark":   "#191970",
+		},
+	}
+
+	err := wxsms.SendMsgWithTemplate(template, "omHgCwm_DFWghRVayaJ35AggBLG8",
+		"加油卡*123456充值成功", "充值网点", "额度账收款", "1000元", "2014-09-22 08:10", "感谢您的使用。")
 	if err != nil {
 		t.Error(err)
 		return
