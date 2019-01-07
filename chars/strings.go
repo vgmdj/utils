474b285c91bcab93d885b43552a11292ac4fa362
vgmdj <228226928@qq.com>
2019-01-07 15:25:21 +0800

    add case

--- chars/strings.go
+++ chars/strings.go
@@ -7,6 +7,7 @@ import (
 )
 
 //Replace 将给定的str，从offset位开始，一直替换limit位的repStr
+//例chars.Replace(“18912345678“,4,4,"*") = “189****5678”
 func Replace(str string, offset int, limit int, repStr string) (result string, err error) {
 	if len(str) < 1 || len(repStr) != 1 || offset+limit-1 > len(str) {
 		err = errors.New("字符长度不合法或函数使用错误")
